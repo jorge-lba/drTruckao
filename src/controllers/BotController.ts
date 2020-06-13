d483b63867e1a796c3d10036f1c55eb383571710
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-13 17:36:14 -0300

    Add - Verificar se o usuario ja foi cadastrado

--- src/controllers/BotController.ts
+++ src/controllers/BotController.ts
@@ -2,11 +2,16 @@ import {Request, Response} from 'express'
 
 import {twilioClient, TwilioMessagingResponse} from '../twilio/index'
 import watsonSendMessage from '../watson/index'
+import User from '../models/User'
+import UserController from './UserController'
 
 export default{
     async response(request:Request, response:Response){
         const numberUSer = request.body.To.replace('whatsapp:','')
         const messageUser = request.body.Body
+
+        const [user] = await User.find({registrationData:{cellPhone:numberUSer}})
+        console.log(user)
 
         console.log(numberUSer, messageUser)               
         const twiml = new TwilioMessagingResponse()
